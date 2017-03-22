850c35969736f66f4e44cb98df3e47403c28bf15
Alon Fliess <alonf@codevalue.net>
2017-03-23 00:15:56 +0200

    Add project files.

---
+++ CSharp7Demos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp7Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
